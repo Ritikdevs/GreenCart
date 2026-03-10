dfd78d71b861265aeb07c6293cb925510c1795c7
RitikDevs <uttamkhanna620@gmail.com>
2026-03-10 11:00:23 -0400

    update code

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import assets, { userDummyData } from '../assets/assets'
+import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import { ChatContext } from '../../context/ChatContext';
+import { useEffect,useState } from 'react';
 
-const Sidebar = ({ selectedUser, setSelectedUser }) => {
+const Sidebar = () => {
 
-  const {logout}= useContext(AuthContext);
+  const {getUsers,users,selectedUser,setSelectedUser,unseenMessages,setUnseenMessages}= useContext(ChatContext);
+
+
+  const {logout,onlineUsers}= useContext(AuthContext);
+  const [input,setInput]=useState(false);
   
   const navigate = useNavigate();
+
+  const filteredUsers= input?users.filter((user)=>user.fullName.toLowerCase().includes(input.toLowerCase())) :users;
+
+  useEffect(()=>{
+   getUsers()
+  },[onlineUsers])
 
   return (
     <div
@@ -54,6 +66,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
         <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
           <img src={assets.search_icon} alt="Search" className="w-3" />
           <input
+           onChange={(e)=>setInput(e.target.value)}
             type="text"
             placeholder="Search User..."
             className="bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1"
@@ -63,10 +76,10 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 
       {/* Users List */}
       <div className="flex flex-col">
-        {userDummyData.map((user, index) => (
+        {filteredUsers.map((user, index) => (
           <div
             key={index}
-            onClick={() => setSelectedUser(user)}
+            onClick={() => { setSelectedUser(user);setUnseenMessages(prev=>({...prev,[user._id]:0}))}}
             className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
               selectedUser?._id === user._id
                 ? "bg-[#282142]/50"
@@ -81,16 +94,18 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 
             <div className="flex flex-col leading-5">
               <p>{user.fullName}</p>
-              {index < 3 ? (
+              {
+              onlineUsers.includes(user._id)
+               ? (
                 <span className="text-green-400 text-xs">Online</span>
               ) : (
                 <span className="text-neutral-400 text-xs">Offline</span>
               )}
             </div>
 
-            {index > 2 && (
+            { unseenMessages[user._id]>0  && (
               <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
-                {index}
+                { unseenMessages[user._id]}
               </p>
             )}
           </div>
