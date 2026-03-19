5ac66ed64e3a5bc0da7217cfc5d2a13955497d38
RitikDevs <uttamkhanna620@gmail.com>
2026-03-19 07:52:23 -0400

    Added seller pages, cart, orders and updated components

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -10,12 +10,12 @@ export const AppContextProvider=({children})=>{
     const currency= import.meta.env.VITE_CURRENCY;
 
     const navigate = useNavigate();
-    const [user,setUser]= useState(false);
+    const [user,setUser]= useState(true);
     const [isSeller,setIsSeller]= useState(false);
     const [showUserLogin,setShowUserLogin]= useState(false);
     const[products,setProducts]=useState([]);
     const [cartItems,setCartItems]=useState({});
-    const[searchQuery,setSearchQuery]= useState({})
+    const[searchQuery,setSearchQuery]= useState("")
     
 
 
@@ -59,8 +59,31 @@ export const AppContextProvider=({children})=>{
         delete cartData[itemId];
     }
   }
-   toast.success("Message Remove from cart");
+   toast.success("Remove from cart");
    setCartItems(cartData)
+ }
+
+ //get cart item count
+ const getCartCount=()=>{
+    let totalCount=0;
+    for(const item in cartItems){
+        totalCount+=cartItems[item];
+
+    }
+    return totalCount;
+ }
+
+ //get cart total amount
+
+ const getCartAmount=()=>{
+    let totalAmount=0;
+    for(const items in cartItems){
+    let itemInfo=products.find((product)=>product._id===items);  
+    if(cartItems[items]>0){
+        totalAmount+= itemInfo.offerPrice*cartItems[items]
+    }
+    }
+    return Math.floor(totalAmount*100)/100;
  }
 
     useEffect(()=>{
@@ -68,10 +91,12 @@ export const AppContextProvider=({children})=>{
     },[])
 
 
+
+
     const value = {
        navigate,
        user,setUser,setIsSeller,isSeller,showUserLogin,setShowUserLogin,products,currency,
-       addToCart,updateCartItem,removeFromCart,cartItems,currency,searchQuery,setSearchQuery
+       addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery,getCartAmount,getCartCount
     }
    return <AppContext.Provider value={value}>
     {children}
