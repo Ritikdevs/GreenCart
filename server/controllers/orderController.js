cffc9c843d0331a7f24df8b48fc091ae81d21e3e
RitikDevs <uttamkhanna620@gmail.com>
2026-03-20 11:45:55 -0400

    Added backend server with routes, controllers and configs

---
+++ server/controllers/orderController.js
@@ -0,0 +1,50 @@
+import Order from "../models/order.js";
+import Product from "../models/product.js";
+
+//place order COD:/api/order/cod
+export const placeOrderCOD =async(req,res)=>{
+  try {
+    const {userId,items,address}=req.body;
+    if(!address || items.length===0){
+        return res.json({success:false,message:"Invalid Data"})
+    }
+
+    //calculate amount using items
+    let amount = await items.reduce(async(acc,item)=>{
+        const product= await Product.findById(item.product);
+        return (await acc) + product.offerPrice * item.quantity;
+
+    },0)
+    //Add tax charge (2%)
+    amount+=Math.floor(amount*0.02);
+    await Order.create({
+        userId,
+        items,
+        amount,
+        address,
+        paymentType:"COD",
+    })
+    return res.json({success:true,message:"Oredr placed successfully"})
+  } catch (error) {
+             console.log(error.message);
+             res.json({success:false,message:error.message})
+  }
+}
+
+//Get orders by User ID :/api/order/user
+
+export const getUserOrders = async(req,res)=>{
+  try {
+    const {userId}= req.body;
+    const orders=await Order.find({
+        userId,
+        $or:[{paymentType:"COD"},{isPaid:true}]
+    }).populate("items.product address").sort({createdAt:-1})
+    res.json({success:true,orders})
+  } catch (error) {
+             console.log(error.message);
+             res.json({success:false,message:error.message})
+  }
+};
+
+//
